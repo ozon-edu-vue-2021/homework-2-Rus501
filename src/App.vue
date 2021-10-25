7e365c7769389589cc2b376e57f1b98d66f0d332
Rus_I <r.angaev9@gmail.com>
2021-10-25 23:38:02 +0300

    Finished hw

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo-img" alt="Vue logo" src="./assets/logo.png" />
+
+    <div class="tree-component">
+      <render-component
+        v-for="(item, i) in items"
+        :key="i"
+        :item="item"
+        :shown="shown"
+        @unfold="unfoldFolder"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as items from '../public/static/node_modules.json'
+import RenderComponent from './components/MainComponent/RenderComponent.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { RenderComponent },
+  data: () => ({
+    items,
+    shown: false,
+  }),
+  methods: {
+    unfoldFolder(item) {
+      console.log(item)
+    },
+  },
 }
 </script>
 
@@ -21,8 +38,14 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-size: 22px;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+.logo-img {
+  width: 100px;
+  display: block;
+  margin: auto;
 }
 </style>
---
+++ src/components/MainComponent/RenderComponent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div :shown="shown" @click.stop="unfoldFolder($event)" class="wrapper">
+    <folder-icon v-if="item[propType] === 'directory'" />
+    <file-icon v-if="item[propType] === 'file'" />
+    <link-icon v-if="item[propType] === 'link'" />
+    {{ item[propName] }}
+    <div
+      v-if="shown && item.contents && item.contents.length > 0"
+      class="subtree"
+    >
+      <render-component
+        v-for="(item, i) in item.contents"
+        :key="i"
+        :item="item"
+        prop-type="type"
+        prop-name="name"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import FolderIcon from '../Icons/FolderIcon.vue'
+import FileIcon from '../Icons/FileIcon.vue'
+import LinkIcon from '../Icons/LinkIcon.vue'
+
+export default {
+  components: { FolderIcon, FileIcon, LinkIcon },
+  name: 'RenderComponent',
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+    propType: {
+      type: String,
+      default: 'type',
+    },
+    propName: {
+      type: String,
+      default: 'name',
+    },
+    contents: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data: () => ({
+    shown: false,
+  }),
+  methods: {
+    unfoldFolder(event) {
+      this.item[this.propType] === 'directory'
+        ? (this.shown = !this.shown)
+        : window.getSelection().selectAllChildren(event.target)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.wrapper {
+  cursor: pointer;
+}
+
+.subtree {
+  margin-left: 15px;
+}
+</style>
